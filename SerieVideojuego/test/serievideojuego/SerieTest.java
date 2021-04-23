0908170abd618ce354bf4f9f7da75e054f392773
Javi <javiersmr471@gmail.com>
2021-04-23 11:52:31 +0200

    Tests

---
+++ SerieVideojuego/test/serievideojuego/SerieTest.java
@@ -0,0 +1,59 @@
+package serievideojuego;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SerieTest {
+
+	@Test
+	void testGetTitulo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetnumeroTemporadas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetGenero() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetcreador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEntregar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDevolver() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsEntregado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsSerie() {
+		fail("Not yet implemented");
+	}
+
+}
